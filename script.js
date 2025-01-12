0dc354a031c420d3ec1df07755d7ced7193ef8d9
clubmbusan <clubmbusan@naver.com>
2025-01-13 01:08:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -542,11 +542,10 @@ function calculateGroupMode(totalAssetValue) {
     }).filter(Boolean); // 누락된 항목 제거
   
     // 결과 출력 부분
- document.getElementById('result').innerHTML = `
+        document.getElementById('result').innerHTML = `
     <h3>계산 결과 (전체 상속)</h3>
     ${heirs
-        .map(
-            (heir) => `
+        .map((heir) => `
             <p>
                 <strong>${heir.name}</strong>: ${heir.shareAmount.toLocaleString()} 원<br>
                 공제 내역:<br>
@@ -557,9 +556,7 @@ function calculateGroupMode(totalAssetValue) {
                 과세 금액: ${heir.taxableAmount.toLocaleString()} 원<br>
                 상속세: ${heir.tax.toLocaleString()} 원
             </p>
-            `
-        )
-        .join('')}
+        `).join('')}
 `;
 
   // 가업 개인 상속 계산을 위한 숫자에 콤마를 추가하는 함수 (가업개인/단체 공통)
