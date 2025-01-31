ef660b87723da6d88b5281c7471224003e3922df
clubmbusan <clubmbusan@naver.com>
2025-01-31 19:56:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -870,8 +870,7 @@ function calculateGroupMode() {
           ${individualResults.join("")}
           <h3>최종 상속세 합계: ${totalInheritanceTax.toLocaleString()} 원</h3>  
       `;
-     }
-
+    
  /**                  
  * ✅ 법정 상속 계산 함수 (민법에 따른 법정 상속 비율 자동 적용)
  * @description 법정 상속 방식으로 상속인의 법정 지분을 자동 계산하고, 
