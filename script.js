9d78c1572760d3de3742840a537bc02698ce99a5
clubmbusan <clubmbusan@naver.com>
2025-01-31 11:01:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -856,8 +856,6 @@ function calculateGroupMode() {
     `;
 }
 
-// ✅ 계산 버튼 클릭 시 실행
-document.getElementById('calculateGroupButton').addEventListener('click', calculateGroupMode);
 
  /**
  * ✅ 법정 상속 계산 함수 (민법에 따른 법정 상속 비율 자동 적용)
