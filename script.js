cb15512e492e0b03a03ce61aa5438f606e75e9bc
clubmbusan <clubmbusan@naver.com>
2025-01-31 11:39:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1076,6 +1076,7 @@ heirs.forEach(heir => {
         ${individualFinancialExemption > 0 ? `<p>금융재산 공제: ${individualFinancialExemption.toLocaleString()} 원</p>` : ""}
         <p>기초 공제: ${individualBasicExemption.toLocaleString()} 원</p>
         <p>관계 공제: ${individualRelationshipExemption.toLocaleString()} 원</p>
+        ${relationship === "spouse" ? `<p>배우자 추가 공제: ${individualSpouseAdditionalExemption.toLocaleString()} 원</p>` : ""}
         ${relationship !== "spouse" ? `<p>일괄 공제 보정액: ${individualLumpSumExemption.toLocaleString()} 원</p>` : ""}
         <p>과세 표준: ${individualTaxableAmount.toLocaleString()} 원</p>
         <p>개별 상속세: ${individualTax.toLocaleString()} 원</p>
