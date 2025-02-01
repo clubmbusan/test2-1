4ddceaf2c82440bb25314d3a7c77ef11fdbe6ae5
clubmbusan <clubmbusan@naver.com>
2025-02-01 13:40:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -811,7 +811,6 @@ function calculateGroupMode() {
             basicExemption,
             financialExemption: individualFinancialExemption,
             lumpSumExemption: individualLumpSumExemption,
-            spouseTransferredExemption,
             finalTaxableAmount,
             individualTax
         };
