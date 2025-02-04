30afe36ecd9c67ff4dcb97a888ab37e4c0ffb97b
clubmbusan <clubmbusan@naver.com>
2025-02-04 12:10:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -906,7 +906,7 @@ let processedHeirs = heirs?.map((heir) => {
     let individualLumpSumExemption = (lumpSumExemption * heir.sharePercentage) / 100 || 0; // 🔥 undefined 방지
 
     // 🔥 추가: 금융재산 공제 변수 선언 및 초기화
-    let individualFinancialExemption = (maxFinancialExemption * heir.sharePercentage) / 100 || 0; // 🔥 undefined 방지
+    individualFinancialExemption = (maxFinancialExemption * heir.sharePercentage) / 100 || 0; // 🔥 undefined 방지
 
     // 🔥 반올림 처리
     spouseTransferredExemption = Math.round(spouseTransferredExemption);
