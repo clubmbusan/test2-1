9b58660314d01b02836d8abc05ba3f82f8db9bf6
clubmbusan <clubmbusan@naver.com>
2025-02-07 23:38:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1681,7 +1681,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 break;
         }
     });
-       
+ });
+    
 // 숫자 포맷 함수
 document.addEventListener('input', (event) => {
     const target = event.target;
